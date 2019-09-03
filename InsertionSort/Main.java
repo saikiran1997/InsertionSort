e0231a566a1a3b9ef791f1fa82dd408b13ae074d
Saikiran Vaidhyanathan <saikiran.nathan@gmail.com>
2019-09-03 14:57:02 -0700

    Created main file added a print statement deleted first print added 2 new print statements implemented scanner - haven't done anything with it yet

--- InsertionSort/Main.java
+++ InsertionSort/Main.java
@@ -1,9 +1,14 @@
 package InsertionSort;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Saikiran");
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Welcome to insertion sort!");
+        System.out.println("\n Enter in the number of elements that you want to sort");
+
+
     }
 }
