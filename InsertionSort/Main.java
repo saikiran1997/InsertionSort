779d2183137003a1689cd55ee2d4fe55cbc84d89
Saikiran Vaidhyanathan <saikiran.vaidhyanathan@sjsu.edu>
2019-09-04 13:29:52 -0700

    added a scanner message

--- InsertionSort/Main.java
+++ InsertionSort/Main.java
@@ -8,6 +8,8 @@ public class Main {
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Welcome to insertion sort!");
         System.out.println("\n Enter in the number of elements that you want to sort");
+        int sizeofArray = keyboard.nextInt();
+        System.out.println("The size of your array is "+ sizeofArray);
 
 
     }
